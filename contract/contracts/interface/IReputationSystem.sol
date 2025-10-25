1f49560c9d083dc7aa34053e737009056f21ffa5
devanshdhruve <dhruvedevansh@gmail.com>
2025-10-25 21:58:11 +0530

    removing smart contracts

--- contract/contracts/interface/IReputationSystem.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-interface IReputationSystem {
-    function getReputation(address user) external view returns (uint256);
-    function awardReputation(address user, uint256 amount) external;
-}
